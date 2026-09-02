6b3a8cfee52a69ba5ff24ba8429b7be6068f5592
chatturock <chattanatha006@gmail.com>
2026-09-02 13:05:00 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,12 +1,8 @@
 def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial not defined for negative numbers")
-    result = 1
-    for i in range(2, n + 1):
-        result *= i
-    return result
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    return fact
 
-if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    print(f"Factorial of {num} is {factorial(num)}") 
-    #Python Program
+num = 5
+print("Factorial of", num, "=", factorial(num))
